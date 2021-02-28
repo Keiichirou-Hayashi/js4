3e3e8703559511688b8b505b0ab95ebe9fab491e
EC2 Default User <ec2-user@ip-172-31-27-34.us-east-2.compute.internal>
2021-02-28 07:27:38 +0000

    first commit

---
+++ slick_slide/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $(".slick01").slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 603,
+        settings: {
+          dots: false,
+        }
+      },
+    ]
+  });
+  });
+    
